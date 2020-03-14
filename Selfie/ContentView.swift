d51bb69f83850d254b1e37ae0b6bfe7b24ca4de6
Tracy Chen <stchen1012@gmail.com>
2020-03-14 12:07:21 -0400

    design added

---
+++ Selfie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Selfie
+//
+//  Created by Tracy Chen on 3/14/20.
+//  Copyright © 2020 Tracy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello")
+        
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Selfie/reflectionsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  reflectionsViewController.swift
+//  Selfie
+//
+//  Created by Tracy Chen on 3/14/20.
+//  Copyright © 2020 Tracy. All rights reserved.
+//
+
+import Foundation
